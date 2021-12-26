e75e1210d459aec1ffc2cf1320d8793cac0c096c
yash621 <yg17381@gmail.com>
2021-12-26 19:03:17 +0530

    implemted cart to api

---
+++ src/components/CartToAPI/CartToAPI.tsx
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from "react";
+
+import { OBJtoAPI } from "../../Redux/ChildrensSlice";
+import ChildrenImage from "../ChildrenImage/ChildrenImage";
+import SmallSpinner from "../Spinner/SmallSpinner";
+import CartToAPICSS from "./CartToAPI.module.css";
+
+interface ICartToAPI {
+  usercart: OBJtoAPI;
+  type: TypeContainer;
+}
+interface Iitem {
+  id: number;
+  _id: string;
+  products: any; // APi returns empty arr of this sinc eits not saving to the DB
+}
+type TypeContainer = "approved" | "discarded";
+const CartToAPI: React.FC<ICartToAPI> = ({ usercart, type }) => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [item, setItem] = useState<Iitem>({ id: 0, _id: "", products: [] });
+  const [error, setError] = useState<boolean>(false);
+
+  useEffect(() => {
+    const abortCont = new AbortController();
+    const postToAPI = () => {
+      setLoading(true);
+      fetch("https://fakestoreapi.com/carts", {
+        method: "POST",
+        signal: abortCont.signal,
+        body: JSON.stringify(usercart),
+      })
+        .then((res) => res.json())
+        .then((json) => {
+          setItem(json);
+          setLoading(false);
+        })
+        .catch((err) => {
+          if (err.name !== "AbortError") {
+            setError(true);
+          }
+        });
+    };
+    postToAPI();
+    return () => abortCont.abort();
+  }, [usercart]);
+
+  if (loading) {
+    return (
+      <div
+        className={
+          type === "approved"
+            ? CartToAPICSS.containerGreen
+            : CartToAPICSS.containerRed
+        }
+      >
+        <ChildrenImage name={usercart.userId} />
+        <SmallSpinner />
+      </div>
+    );
+  } else if (error) {
+    return (
+      <div
+        className={
+          type === "approved"
+            ? CartToAPICSS.containerGreen
+            : CartToAPICSS.containerRed
+        }
+      >
+        <ChildrenImage name={usercart.userId} />
+        <span className={CartToAPICSS.error}>Error , not saved!</span>
+      </div>
+    );
+  }
+  return (
+    <div
+      className={
+        type === "approved"
+          ? CartToAPICSS.containerGreen
+          : CartToAPICSS.containerRed
+      }
+    >
+      <ChildrenImage name={usercart.userId} />
+      <span className={CartToAPICSS.success}>successfully saved!</span>
+
+      <span>
+        {" "}
+        <span style={{ fontWeight: 800 }}>ID:</span>
+        {item._id}
+      </span>
+    </div>
+  );
+};
+
+export default CartToAPI;
